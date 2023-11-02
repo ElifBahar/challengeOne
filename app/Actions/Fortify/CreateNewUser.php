4dc45c7097c6fb30dc14beb0bec15fac83bcf2ab
ElifBahar <elif.har94@gmail.com>
2023-11-02 16:16:00 +0300

    yeni kullanıcı oluşturma alanları kayıt fonksiyonu oluşturuldu

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -21,13 +21,20 @@ class CreateNewUser implements CreatesNewUsers
     {
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
+            'tc' => ['required', 'string', 'max:11', 'min:11'],
+            'school_number' => ['required', 'string', 'max:9', 'min:9'],
+            'surname' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
         return User::create([
+            'tc' => $input['tc'],
             'name' => $input['name'],
+            'surname' => $input['surname'],
+            'school_number' => $input['school_number'],
+            'type' => 1,
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
--- app/Models/User.php
+++ app/Models/User.php
@@ -25,6 +25,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
+        'tc',
+        'school_number',
+        'type',
         'email',
         'password',
     ];
